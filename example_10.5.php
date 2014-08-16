bd9531225507478340d16ea393b3f41b29762ba7
virtual-marat <virtual.marat@gmail.com>
2014-08-16 21:34:21 +0600

    Поячеечное извлечение результата

--- example_10.5.php
+++ example_10.5.php
@@ -2,6 +2,8 @@
 require_once('login.php');
 require_once('mysql_fatal_error.php');
 
+echo 'Это файл example_10.5.php';
+
 $db_server = mysql_connect($db_hostname, $db_username, $db_password);
 if (!$db_server) die(mysql_fatal_error('Невозможно подключиться к серверу MySQL'));
 
