23a596a261527ab84db6911346b6166c416ac303
virtual-marat <virtual.marat@gmail.com>
2014-08-16 19:21:49 +0600

    Отправка запроса к базе данных MySQL

--- example_10.4.php
+++ example_10.4.php
@@ -3,7 +3,9 @@
 require_once('login.php');
 require_once('mysql_fatal_error.php');
 
-$db_server = mysql_connect($db_server, $db_username, $db_password);
+echo '<p>' . 'Это файл example_10.4.php' . '</p>';
+
+$db_server = mysql_connect($db_hostname, $db_username, $db_password);
 if(!$db_server)
     die(mysql_fatal_error('Неовзможно подключиться к серверу MySQL'));
 
