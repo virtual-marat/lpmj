e2ae426c8c4d98a9c90728426f4c381154f156f3
virtual-marat <virtual.marat@gmail.com>
2014-08-16 16:34:06 +0600

    Выбор базы данных

--- exmaple_10.3.php
+++ exmaple_10.3.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Marat
- * Date: 16.08.14
- * Time: 14:57
- */
+
 require_once 'login.php';
 require_once 'mysql_fatal_error.php';
 echo '<p>' . 'Это файл example_10.3.php' . '</p>';
