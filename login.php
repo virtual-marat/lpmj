f26b0fd4c100d5841ad75313e2e0f7ab5b7686a4
virtual-marat <virtual.marat@gmail.com>
2014-08-16 16:27:13 +0600

    Файл авторизации

--- login.php
+++ login.php
@@ -1,10 +1,5 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Marat
- * Date: 16.08.14
- * Time: 1:00
- */
+<?php //Файл авторизации
+
 $db_hostname = 'localhost';
 $db_database = 'publications';
 $db_username = 'root';
