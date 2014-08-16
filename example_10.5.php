123fa994b00c68146115970ea9e3763daa687bd7
virtual-marat <virtual.marat@gmail.com>
2014-08-16 20:08:54 +0600

    Поячеечное извлечение результата

--- example_10.5.php
+++ example_10.5.php
@@ -1,6 +1,6 @@
 <?php // Поячеечное извлечение результата
 require_once('login.php');
-require_once('mysql_fatal_error');
+require_once('mysql_fatal_error.php');
 
 $db_server = mysql_connect($db_hostname, $db_username, $db_password);
 if (!$db_server) die(mysql_fatal_error('Невозможно подключиться к серверу MySQL'));
@@ -9,14 +9,14 @@ mysql_select_db($db_database)
     or die(mysql_fatal_error('Невозможно выбрать базу данных MySQL'));
 
 $query = 'SELECT * FROM classics';
-$result = $mysql_query($query);
+$result = mysql_query($query);
 if (!$result) die(mysql_fatal_error('Сбой при доступе к базе данных'));
 
 $row = mysql_num_rows($result);
 for($j = 0; $j < $row; $j++) {
-    echo 'Автор: ' . mysql_result($result, j) . '<br />';
-    echo 'Название: ' . mysql_result($result, j) . '<br />';
-    echo 'Категория: ' . mysql_result($result, j) . '<br />';
-    echo 'Год издания: ' . mysql_result($result, j) . '<br />';
-    echo 'ISBN: ' . mysql_result($result, j) . '<br />';
+    echo 'Автор: ' . mysql_result($result, $j, 'author') . '<br />';
+    echo 'Название: ' . mysql_result($result, $j, 'title') . '<br />';
+    echo 'Категория: ' . mysql_result($result, $j, 'category') . '<br />';
+    echo 'Год издания: ' . mysql_result($result, $j, 'year') . '<br />';
+    echo 'ISBN: ' . mysql_result($result, $j, 'isbn') . '<br />' . '<br />';
 }
