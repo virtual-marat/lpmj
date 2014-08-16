cbf51e160831681b8dc54fe6deb8e8dacf1e00ad
virtual-marat <virtual.marat@gmail.com>
2014-08-16 19:04:13 +0600

    Отправка запроса к базе данны�

--- example_10.4.php
+++ example_10.4.php
@@ -1,3 +1,17 @@
 <?php // Отправка запроса к базе данных
 
-$query
+require_once('login.php');
+require_once('mysql_fatal_error.php');
+
+$db_server = mysql_connect($db_server, $db_username, $db_password);
+if(!$db_server)
+    die(mysql_fatal_error('Неовзможно подключиться к серверу MySQL'));
+
+mysql_select_db($db_database)
+    or die(mysql_fatal_error('Невозможно подключиться к базе данных MySQL'));
+
+$query = 'SELECT * FROM classics';
+$result = mysql_query($query);
+
+if(!$result)
+    die(mysql_fatal_error('Сбой при доступе к базе данных'));
