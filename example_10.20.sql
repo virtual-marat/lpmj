eb845e50bd20c30ea4a969bb5db264ae394c9ae9
virtual-marat <virtual.marat@gmail.com>
2014-09-07 01:15:07 +0600

    Использование указателей мест заполнения

---
+++ example_10.20.sql
@@ -0,0 +1,13 @@
+# Использование указателей мест заполнения
+
+PREPARE statement FROM 'INSERT INTO classics VALUES(?, ?, ?, ?, ?)';
+
+SET @author   = 'Emily Bronte';
+    @title    = 'Wuthering Heights';
+    @category = 'Classic Fiction';
+    @year     = '1847';
+    @isbn     = '9780553212587';
+
+EXECUTE statement USING @author, @title, @category, @year, @isbn;
+
+DEALLOCATE PREPARE statement;
