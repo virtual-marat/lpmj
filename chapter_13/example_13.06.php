f362aa1140ed5d030495cdfdec04f999d8883bb2
virtual-marat <virtual.marat@gmail.com>
2014-12-14 01:35:15 +0500

    Извлечение переменных сессии

---
+++ chapter_13/example_13.06.php
@@ -0,0 +1,17 @@
+<?php // Извлечение переменных сессии
+session_start();
+
+if (isset($_SESSION['username']))
+{
+    $username   = $_SESSION['username'];
+    $password   = $_SESSION['password'];
+    $forename   = $_SESSION['forename'];
+    $surname    = $_SESSION['surname'];
+
+    echo "С возвращением, $forename. <br />
+      Ваше полное имя $forename $surname.
+      Ваше имя пользователя $username
+      и ваш пароль $password.";
+}
+else
+    echo "Пожалуйста, для входа <a href=\"example_13.05.php\">щелкните здесь</a>.";
