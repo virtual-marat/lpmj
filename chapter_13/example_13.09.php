0d5e0200e35626fbfee18bb0fc66a4b5aae792b4
virtual-marat <virtual.marat@gmail.com>
2015-01-19 00:53:19 +0500

    Сессия, восприимчивая к фиксации сессии

---
+++ chapter_13/example_13.09.php
@@ -0,0 +1,6 @@
+<?php // Сессия, восприимчивая к фиксации сессии
+session_start();
+if (!isset($_SESSION['count'])) $_SESSION['count'] = 0;
+else
+    ++$_SESSION['count'];
+echo $_SESSION['count'];
